3f40d922edd6eb5c6f5d316063a34df258fdda7d
colinator27 <colinator27@users.noreply.github.com>
2016-10-24 15:50:31 -0400

    Fix + audio support

--- GMSDumper/Program.cs
+++ GMSDumper/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -11,15 +11,33 @@ namespace GMSDumper
     {
         static void Main(string[] args)
         {
+            List<byte[]> list3 = Audio.getAudio();
             List<byte[]> list2 = Strings.getStrings();
             List<byte[]> list = Textures.getTextures();
             Console.WriteLine("Exporting images...");
             if (list != null)
             for (int i = 0; i < list.Count; i++){
-                File.WriteAllBytes(AppDomain.CurrentDomain.BaseDirectory + "/texture"+i.ToString()+".png", list[i]);
+                try {
+                    File.WriteAllBytes(AppDomain.CurrentDomain.BaseDirectory + "/texture" + i.ToString() + ".png", list[i]);
+                } catch (IOException e)
+                {
+                    Console.WriteLine(e.Message + "\n Failed to write file.");
+                }
                 Console.WriteLine("Exporting image "+i.ToString());
             }
-            Console.WriteLine("Exporting STRG");
+            Console.WriteLine("Exporting audio...");
+            if (list3 != null)
+            for (int i = 0; i < list3.Count; i++)
+            {
+                try {
+                    File.WriteAllBytes(AppDomain.CurrentDomain.BaseDirectory + "/sound_" + i.ToString() + ".wav", list3[i]);
+                } catch (IOException e)
+                {
+                    Console.WriteLine(e.Message + "\n Failed to write file.");
+                }
+                Console.WriteLine("Exporting audio " + i.ToString());
+            }
+            Console.WriteLine("Exporting strings...");
             File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "/strings.txt", String.Empty);
             if (list2 != null)
             for (int i = 0; i < list2.Count; i++)
